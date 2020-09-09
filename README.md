a94cfcd6dc199ac8c2044d0c5b4586755ed7272f
Robert Easley <roberteasley@Roberts-Mac-mini.local>
2020-09-09 14:37:17 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Super Basic List Orderer For Local Grocery Stores
+
+During the pandemic my wife and I were regularly ordering our shoping lists to minimize the amount of time we spent in grocery stores. I wrote this very simple program that takes our shoping lists, fuzzy matches items, and auto orders them based on the layout of our local grovery stores. This is likely useless to everyone except us. Written in a couple hours so not qaulity code.
+
+[DEMO](https://robertpage.github.io/order-grocery-lists/)
+
+## To Run
+npm install
+npm run dev
---
+++ src/scripts/index.js
@@ -0,0 +1,89 @@
+import { match } from 'fuzzyjs'
+import { lists } from './modules/data/grocery-lists';
+
+const input = document.querySelector(".list-input");
+const generateTradersListButton = document.querySelector(".generate-ordered-traders-list-button");
+const generateWfListButton = document.querySelector(".generate-ordered-wf-list-button");
+const output = document.querySelector(".ordered-list-output");
+
+window.onload = function() {
+    generateTradersListButton.addEventListener("click", ()=>{init("traders")}, false);
+    generateWfListButton.addEventListener("click", ()=>{init("wholeFoods")}, false);
+};
+function init (store) {
+    const numberOfItems = {};
+    const foodsArray = Object.keys(lists[store]);
+
+    const matchedItemsList = [];
+    const unmatched = [];
+
+    if (input.value.length < 1) {
+        alert("You must add items to the grocery list")
+        return;
+    }
+
+    const listArray = input.value.split("\n");
+    let number;
+    for (let i = 0; i < listArray.length; i++) {
+        let topMatch;
+        let closestDistance;
+        for (let j = 0; j < foodsArray.length; j++) {
+            number = getNumberOfItems(listArray[i]);
+            const listItem = cleanWord(listArray[i]);
+            const scoreObj = match(listItem, foodsArray[j]);
+
+            if (!closestDistance || closestDistance < scoreObj.score) {
+                closestDistance = scoreObj.score;
+                topMatch = foodsArray[j];
+            }
+
+        }
+        if (closestDistance > 9) {
+            if (number) numberOfItems[topMatch] = number;
+            matchedItemsList.push(topMatch);
+        } else {
+            unmatched.push(listArray[i]);
+        }
+
+    }
+
+    const zonedFoods2dArray = [[], [], [], [], [], [], [], [], [], [], [], []];
+    for (let k = 0; k < matchedItemsList.length; k++) {
+        if (numberOfItems[matchedItemsList[k]]) {
+            zonedFoods2dArray[lists[store][matchedItemsList[k]]].push(numberOfItems[matchedItemsList[k]]+ " " +matchedItemsList[k])
+        } else {
+            zonedFoods2dArray[lists[store][matchedItemsList[k]]].push(matchedItemsList[k])
+        }
+
+    }
+
+    let finalList = "";
+    for (let l = 0; l < zonedFoods2dArray.length; l++) {
+        if (zonedFoods2dArray[l].length > 0) {
+            const element = zonedFoods2dArray[l].join("\n");
+            finalList = finalList + element + "\n\n\n-\n";
+        }
+
+    }
+    if (unmatched.length > 0) {
+        finalList = finalList + "UNMATCHED ITEMS:\n" + unmatched.join("\n")
+    }
+
+    output.value = finalList;
+}
+
+
+function cleanWord (string) {
+    const regex = /^[0-9].*?\s/;
+    let tmpString = string.toLowerCase();
+    tmpString = tmpString.trim();
+    return tmpString.replace(regex, "");
+}
+
+function getNumberOfItems (string) {
+    if (/^[0-9]/.test(string)) {
+        const regex = /^[0-9].*?\s/;
+        return string.match(regex);
+    }
+    return;
+}
